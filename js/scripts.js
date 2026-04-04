08523dfa7b14e9a74e802ce2c5d952a89a193196
VisiGoyo <79362138+gvisiedo@users.noreply.github.com>
2026-04-04 23:22:56 +0200

    working in JS

--- js/scripts.js
+++ js/scripts.js
@@ -4,31 +4,47 @@ const listaTareas = document.querySelector('#lista-tareas');
 
 btnAñadir.addEventListener('click', ()=>añadirTarea());
 
-function añadirTarea(texto = inputTarea.value){
-    if(texto !== ''){
+function añadirTarea(tarea={texto:inputTarea.value, completada:false}){
+    if(tarea.texto !== ''){
         const li = document.createElement('li'); //crea el elemnto
-        li.textContent = texto; //le pone el texto
+        li.textContent = tarea.texto; //le pone el texto
+        if(tarea.completada)li.classList.add('completada');
         listaTareas.appendChild(li); //lo añade a la lista
         inputTarea.value = ''; // limpia el input después de añadir
+        
         const btnEliminar = document.createElement('button');
         btnEliminar.textContent = 'X';
-        btnEliminar.addEventListener('click', ()=> li.remove());
-        li.appendChild(btnEliminar);
-        li.addEventListener('click', ()=>li.classList.toggle('completada'));
         btnEliminar.addEventListener('click', (e)=>{
             e.stopPropagation();
             li.remove();
+            guardarEnStorage();
+            });
+        li.appendChild(btnEliminar);
+        li.addEventListener('click', ()=>{
+            console.log('ANTES del toggle:', li.classList.contains('completada'));
+            li.classList.toggle('completada');
+            console.log('Despues del toggle:', li.classList.contains('completada'));
+                       
         });
+        guardarEnStorage();
+        }
+        };
         //al final guarda en storage
-        const tareas = Array.from(listaTareas.querySelectorAll('li')).map(li =>li.childNodes[0].textContent);
-        guardarEnStorage(tareas);
-    };
+        function guardarEnStorage(){
+        const tareas = Array.from(listaTareas.querySelectorAll('li')).map(elemento =>{
+            console.log('clase del elemento:', elemento.className);
+            return{texto: elemento.childNodes[0].textContent,
+                completada: elemento.classList.contains('completada')
+        };
+        console.log('guardando:', tareas);
+    });
+        localStorage.setItem('tareas', JSON.stringify(tareas) );
+       
+    
 }
 
-function guardarEnStorage(tareas){
-    localStorage.setItem('tareas', JSON.stringify(tareas) );
 
-}
+
 function cargarDeStorage(){
     const tareas = JSON.parse(localStorage.getItem('tareas')) || [];
     tareas.forEach(tarea => añadirTarea(tarea));
